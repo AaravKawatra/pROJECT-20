0d6e330ff0bb72542d417fe4034e61bbce904821
AaravKawatra <85671252+AaravKawatra@users.noreply.github.com>
2021-07-29 14:03:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function preload(){
+bg= loadImage("") 
+
+}
+
+
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+  
+} 
